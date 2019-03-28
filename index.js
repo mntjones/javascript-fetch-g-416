4882f7a3754088d63c86ca3a6c82b6e0f4441d31
Monica Tamaru Jones <mntamaru@gmail.com>
2019-03-28 00:05:08 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 const app = "I don't do much.";
+fetch('https://api.github.com/repos/jquery/jquery/commits')
+  .then(res => res.json())
+  .then(json => console.log(json));
+  
+// 1st - URL for data
+// 2nd - gets response (res) from fetch and using the json() to turn it into JSON
+// 3rd - then that gets passed to the console.log()
+
